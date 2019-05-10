64b2067e84d6ad91a50ade67a358e1c6cc1e6ec3
EeliKin <50498955+EeliKin@users.noreply.github.com>
2019-05-10 23:37:16 +0300

    VillagePoople
    VillagePoople on se mainifilu, sen kun ajaa niin niin avaa "asiakas" sivun joka jonkun takia on vähän fucky täl hetkel, ei nyt keskittymiskyky riitä tällä hetkellä löytämään vikaa. Luultavasti vika JPanelin kanssa. Pitää huomenna kommentoida loppuun ja yrittää liittää eemin asiakasnäkymätiedosto ja toiminta tähän AasiRuutu.java:an

---
+++ AasiRuutu.java
@@ -0,0 +1,63 @@
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+
+public class AasiRuutu extends JFrame{
+
+    private JPanel aRuutu;
+
+    public static void main(String[] args)
+	{
+		/* It posts an event (Runnable)at the end of Swings event list and is
+		processed after all other GUI events are processed.*/
+		EventQueue.invokeLater(new Runnable()
+		{
+			public void run()
+			{
+				//try - catch block
+				try 
+				{
+					//Kutsuu AasiRuutu constructoria luomaan meille ekan käyttöliittymän
+					AasiRuutu Ruutu = new AasiRuutu();
+					Ruutu.setVisible(true);	//yksiselitteinen			
+				} 
+				catch (Exception e)
+				{
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+
+    public void AasiRuutu()  //constructori jolla luodaan asiakasruutu, periaatteessa identtinen VillagePooplen samanmoiseen
+    {
+        setTitle("Asiakas");
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(500, 500, 750, 750);
+		aRuutu = new JPanel();		//Tuossa JPaneeli mihin laitetaan sisältöä / yritetään luoda asiakkaalle oma ruutu
+        aRuutu.setBorder(new EmptyBorder(5, 5, 5, 5));		//Borderii tolle ruudulle
+		setContentPane(aRuutu);
+        aRuutu.setLayout(null); 
+        
+
+        //set Label in the frame
+		JLabel VPAsia = new JLabel("Alta löydät lisäpalvelut sekä mökkivaihtoehdot");
+		//set foreground color to the label
+		VPAsia.setForeground(Color.BLUE);
+		//set font of that label
+        VPAsia.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 24));
+		//set bound of the label
+		VPAsia.setBounds(100, 50, 750, 39);
+		//add label to the contentPane
+		aRuutu.add(VPAsia);
+    }
+}
---
+++ VillagePoople.java
@@ -0,0 +1,83 @@
+
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+ 
+//Tehdään alkuclassi näytölle
+public class VillagePoople extends JFrame 
+{
+	private JPanel ruutu;	    //Tehdään paneeli meidän käyttöön
+
+	 // Maini jolla itse ohjelma rullaa eteenpäin
+	public static void main(String[] args)
+	{
+		/* It posts an event (Runnable)at the end of Swings event list and is
+		processed after all other GUI events are processed.*/
+		EventQueue.invokeLater(new Runnable()
+		{
+			public void run()
+			{
+				//try - catch block
+				try 
+				{
+					//Kutsuu Village People constructoria luomaan meille ekan käyttöliittymän
+					VillagePoople ruutu = new VillagePoople();
+					ruutu.setVisible(true);	//yksiselitteinen			
+				} 
+				catch (Exception e)
+				{
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+ 
+	// Alla olevalla luodaan se itse ruutu meille käyttöön
+
+	public VillagePoople()									//Tää constructori kasaa meille sen ruudun
+	{
+		setTitle("Village People varausjärjestelmä");
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(500, 500, 750, 750);
+		ruutu = new JPanel();								//Tuossa JPaneeli mihin laitetaan sisältöä
+        ruutu.setBorder(new EmptyBorder(5, 5, 5, 5));		//Borderii tolle ruudulle
+		setContentPane(ruutu);
+    	ruutu.setLayout(null);
+		
+        JButton aNappi = new JButton("Asiakas");			//Luodaan asiakasnappi, lisätään teksti nappiin
+		aNappi.addActionListener(new ActionListener()		//luodaan kuuntelija nappiin, tsekkaa et painellaanko sitä
+		{
+			public void actionPerformed(ActionEvent arg0)
+			{
+				//kutsuu classia AasiRuutu
+				AasiRuutu raamit = new AasiRuutu();
+				raamit.setVisible(true);
+				//set default close operation
+				setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+			}
+		});
+															
+		aNappi.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 12));//set font of the Button
+		aNappi.setBounds(125, 195, 125, 25);				//set bounds of the Button
+		ruutu.add(aNappi);									//add Button into contentPane
+
+
+		//set Label in the frame
+		JLabel VPTerv = new JLabel("Tervetuloa Village People Mokkivarausjarjestelmaan!");
+		//set foreground color to the label
+		VPTerv.setForeground(Color.BLUE);
+		//set font of that label
+        VPTerv.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 24));
+		//set bound of the label
+		VPTerv.setBounds(100, 50, 750, 39);
+		//add label to the contentPane
+		ruutu.add(VPTerv);
+	}
+}
