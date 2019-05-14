399b1c7bb29d5a924a0338a1c05616e18e694b02
mutukebab <44535373+mutukebab@users.noreply.github.com>
2019-05-14 16:11:24 +0300

    Create Varaus.java

---
+++ Varaus.java
@@ -0,0 +1,34 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Time;
+
+public class Varaus {
+	
+	Connection yhteys;
+	
+	// Varaus olion luonti vaatii jo luodun tietokantayhteyden (DBConnection -olio palauttaa True)
+	public Varaus(Connection yhteys) {
+		yhteys = this.yhteys;
+	}
+	
+	// Varauksen lisääminen tällä metodilla, tarvitsee kaikki parametrit
+	public void lisaaVaraus(int asiakasID, int toimipisteID, String palvelu, int palveluLkm, Time varausAika, Time varausAlkaa, Time varausLoppuu) throws SQLException {
+		
+		PreparedStatement stmt = null;
+		
+		try {
+			stmt = yhteys.prepareStatement("INSERT INTO vp.varaus (asiakas_id, toimipiste_id, varattu_pvm, vahvistus_pvm, varattu_alkupvm, varattu_loppupvm) "
+					+ "VALUES (" + asiakasID + ", " + toimipisteID + ", " + varausAika + ", " + varausAlkaa + ", " + varausLoppuu + ");");
+			stmt.executeUpdate();
+			// System.out.println("Varaus tehty onnistuneesti."); // Tulostus jos onnistuu (testi)
+		} catch (SQLException e) {
+			System.out.println(e);
+		} finally {
+			if (stmt != null) {
+				stmt.close();
+			}
+		}
+		
+	}
+}
