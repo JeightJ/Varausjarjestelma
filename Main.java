fe65c954f14f918ffd42aad17f30506d7dc65ee6
mutukebab <44535373+mutukebab@users.noreply.github.com>
2019-05-16 03:22:51 +0300

    Pääohjelma

---
+++ Main.java
@@ -0,0 +1,13 @@
+/* Mökkivarausjärjestelmän pääohjelma, jolla ohjelma käynnistetään */
+
+import java.sql.SQLException;
+
+public class Main {
+
+	public static void main(String[] args) throws SQLException {
+		
+		VillagePoople ruutu = new VillagePoople();
+		ruutu.setVisible(true);
+
+	}
+}
