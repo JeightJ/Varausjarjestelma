57d5ea67c645e1b11883c1546ce1cb8d6f28fdcd
mutukebab <44535373+mutukebab@users.noreply.github.com>
2019-05-17 03:39:09 +0300

    Update Varaus.java

--- mokinvarausjarjestelma/src/mokinvarausjarjestelma/Varaus.java
+++ mokinvarausjarjestelma/src/mokinvarausjarjestelma/Varaus.java
@@ -4,6 +4,9 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Time;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class Varaus {
 	
@@ -14,14 +17,20 @@ public class Varaus {
 		this.yhteys = yhteys;
 	}
 	
-	// Varauksen lis‰‰minen t‰ll‰ metodilla, tarvitsee kaikki parametrit
-	public void lisaaVaraus(int asiakasID, int toimipisteID, String palvelu, int palveluLkm, Time varausAika, Time varausAlkaa, Time varausLoppuu) throws SQLException {
+	// Varauksen lis√§√§minen t√§ll√§ metodilla, tarvitsee kaikki parametrit
+	public void lisaaVaraus(int asiakasID, int toimipisteID, String varausAlku, String varausLoppu) throws SQLException {
 		
+		Date date = new Date();
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		String varausAika = formatter.format(date);
+		String vahvistusAika = formatter.format(date);
+		String varausAlkaa = varausAlku + " 15:00:00";
+		String varausLoppuu = varausLoppu + " 12:00:00";
 		PreparedStatement stmt = null;
 		
 		try {
 			stmt = yhteys.prepareStatement("INSERT INTO vp.varaus (asiakas_id, toimipiste_id, varattu_pvm, vahvistus_pvm, varattu_alkupvm, varattu_loppupvm) "
-					+ "VALUES (" + asiakasID + ", " + toimipisteID + ", " + varausAika + ", " + varausAlkaa + ", " + varausLoppuu + ");");
+					+ "VALUES (" + asiakasID + ", " + toimipisteID + ", '" + varausAika + "', '" + vahvistusAika + "', '" + varausAlkaa + "', '" + varausLoppuu + "');");
 			stmt.executeUpdate();
 			// System.out.println("Varaus tehty onnistuneesti."); // Tulostus jos onnistuu (testi)
 		} catch (SQLException e) {
